3363cc8f367f1f42d263defb386be78f3b0f754a
VPT-bit <64685742+VPT-bit@users.noreply.github.com>
2024-03-15 10:34:27 +0700

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -5,14 +5,14 @@ error() {
     if [ "$#" -eq 2 ]; then
         
         if [[ "$LANG" == zh_CN* ]]; then
-            echo -e \[$(date +%m%d-%T)\] "\033[1;31m"$1"\033[0m"
+            echo -e \[$(date +%d/%m-%T)\] "\033[1;31m"$1"\033[0m"
         elif [[ "$LANG" == en* ]]; then
-            echo -e \[$(date +%m%d-%T)\] "\033[1;31m"$2"\033[0m"
+            echo -e \[$(date +%d/%m-%T)\] "\033[1;31m"$2"\033[0m"
         else
-            echo -e \[$(date +%m%d-%T)\] "\033[1;31m"$2"\033[0m"
+            echo -e \[$(date +%d/%m-%T)\] "\033[1;31m"$2"\033[0m"
         fi
     elif [ "$#" -eq 1 ]; then
-        echo -e \[$(date +%m%d-%T)\] "\033[1;31m"$1"\033[0m"
+        echo -e \[$(date +%d/%m-%T)\] "\033[1;31m"$1"\033[0m"
     else
         echo "Usage: error <Chinese> <English>"
     fi
@@ -22,14 +22,14 @@ yellow() {
     if [ "$#" -eq 2 ]; then
         
         if [[ "$LANG" == zh_CN* ]]; then
-            echo -e \[$(date +%m%d-%T)\] "\033[1;33m"$1"\033[0m"
+            echo -e \[$(date +%d/%m-%T)\] "\033[1;33m"$1"\033[0m"
         elif [[ "$LANG" == en* ]]; then
-            echo -e \[$(date +%m%d-%T)\] "\033[1;33m"$2"\033[0m"
+            echo -e \[$(date +%d/%m-%T)\] "\033[1;33m"$2"\033[0m"
         else
-            echo -e \[$(date +%m%d-%T)\] "\033[1;33m"$2"\033[0m"
+            echo -e \[$(date +%d/%m-%T)\] "\033[1;33m"$2"\033[0m"
         fi
     elif [ "$#" -eq 1 ]; then
-        echo -e \[$(date +%m%d-%T)\] "\033[1;33m"$1"\033[0m"
+        echo -e \[$(date +%d/%m-%T)\] "\033[1;33m"$1"\033[0m"
     else
         echo "Usage: yellow <Chinese> <English>"
     fi
@@ -39,14 +39,14 @@ blue() {
     if [ "$#" -eq 2 ]; then
         
         if [[ "$LANG" == zh_CN* ]]; then
-            echo -e \[$(date +%m%d-%T)\] "\033[1;34m"$1"\033[0m"
+            echo -e \[$(date +%d/%m-%T)\] "\033[1;34m"$1"\033[0m"
         elif [[ "$LANG" == en* ]]; then
-            echo -e \[$(date +%m%d-%T)\] "\033[1;34m"$2"\033[0m"
+            echo -e \[$(date +%d/%m-%T)\] "\033[1;34m"$2"\033[0m"
         else
-            echo -e \[$(date +%m%d-%T)\] "\033[1;34m"$2"\033[0m"
+            echo -e \[$(date +%d/%m-%T)\] "\033[1;34m"$2"\033[0m"
         fi
     elif [ "$#" -eq 1 ]; then
-        echo -e \[$(date +%m%d-%T)\] "\033[1;34m"$1"\033[0m"
+        echo -e \[$(date +%d/%m-%T)\] "\033[1;34m"$1"\033[0m"
     else
         echo "Usage: blue <Chinese> <English>"
     fi
@@ -55,14 +55,14 @@ blue() {
 green() {
     if [ "$#" -eq 2 ]; then
         if [[ "$LANG" == zh_CN* ]]; then
-            echo -e \[$(date +%m%d-%T)\] "\033[1;32m"$1"\033[0m"
+            echo -e \[$(date +%d/%m-%T)\] "\033[1;32m"$1"\033[0m"
         elif [[ "$LANG" == en* ]]; then
-            echo -e \[$(date +%m%d-%T)\] "\033[1;32m"$2"\033[0m"
+            echo -e \[$(date +%d/%m-%T)\] "\033[1;32m"$2"\033[0m"
         else
-            echo -e \[$(date +%m%d-%T)\] "\033[1;32m"$2"\033[0m"
+            echo -e \[$(date +%d/%m-%T)\] "\033[1;32m"$2"\033[0m"
         fi
     elif [ "$#" -eq 1 ]; then
-        echo -e \[$(date +%m%d-%T)\] "\033[1;32m"$1"\033[0m"
+        echo -e \[$(date +%d/%m-%T)\] "\033[1;32m"$1"\033[0m"
     else
         echo "Usage: green <Chinese> <English>"
     fi
